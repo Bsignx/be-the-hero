64348a680e89aab8955977804ceb59836a487752
bsignx <bsignx@gmail.com>
2020-03-29 13:37:31 -0300

    fineshed

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -2,10 +2,21 @@
 
 module.exports = {
 
-    development: {
+  development: {
       client: 'sqlite3',
       connection: {
         filename: './src/database/db.sqlite'
+      },
+      migrations: {
+        directory: './src/database/migrations'
+      },
+      useNullAsDefault: true,
+    },
+
+  test: {
+      client: 'sqlite3',
+      connection: {
+        filename: './src/database/test.sqlite'
       },
       migrations: {
         directory: './src/database/migrations'
